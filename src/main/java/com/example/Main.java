c3b45cb251627b98759207213a7c7753668a0529
Davide Aiazzi <aiazzi.davide@itismeucci.com>
2023-09-26 17:42:41 +0200

    client1

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        ClientStr client = new ClientStr();
+        client.connetti();
+        client.comunica();
+    }
+}
